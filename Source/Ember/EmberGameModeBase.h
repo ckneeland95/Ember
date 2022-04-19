dd80f4d443878255c1fb2b5e49dd3c92246dd4af
ckneeland95 <c.kneeland@outlook.com>
2022-04-19 17:55:13 -0400

    Project Setup

---
+++ Source/Ember/EmberGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EmberGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMBER_API AEmberGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
